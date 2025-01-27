ab927c173c6d5e47a2ef417c43c7e0cf65bd2f39
bloodybear <sincarisebastian@gmail.com>
2025-01-27 22:32:14 +0200

    max flow

--- Algorithms/Graphs/0x0b_max_flow.cpp
+++ Algorithms/Graphs/0x0b_max_flow.cpp
@@ -1,88 +1,103 @@
 #include <bits/stdc++.h>
-#define size_ 100
+#define size_ 10002
+#define maxInt (1<<30)
 using namespace std;
 
-ifstream fin("in.txt");
-ofstream fout("out.txt"); 
+ifstream fin("input.txt");
+ofstream fout("output.txt");
 
 int m, n;
 vector<int> graph[size_ + 1];
 bitset<size_>visited;
-int flux[size_+1][size_+1];
+int flow[size_+1][size_+1];
+int parent[size_+1];
 int capacity[size_+1][size_+1];
-int p[size_+1];
 
-
-int bfs(const int s, const int d){
-    queue<int> q;
-    for (int i =1;i<=n;i+=1){
-        visited[i]=false;
-        p[i]=0;
+void read_graph() {
+    fin >> n >> m;
+    for(int i = 1; i <= m; i++) {
+        int x,y,c;
+        fin >> x >> y >> c;
+        graph[x].emplace_back(y);
+        graph[y].emplace_back(x);
+        capacity[x][y]=c;
     }
-    q.push(s);
-    visited[s]=true;
-    while(!q.empty()){
-        const int node = q.front();
-        q.pop();
-        for(auto node2:graph[node]){
-            if(!visited[node2]){
-                if(capacity[node][node2] - flux[node][node2] > 0){
-                    q.push(node2);
-                    visited[node2] = true;
-                    p[node2] = node;
+}
+
+void reset_visited_parent() {
+    for (int i=1;i<=n;i+=1) {
+        parent[i] = 0;
+        visited[i] = false;
+    }
+}
+
+void bfs(const int source) {
+    queue<int> _queue;
+    _queue.push(source);
+    visited[source] = true;
+    while(!_queue.empty()) {
+        const int front_node = _queue.front();
+        _queue.pop();
+        for (const auto node : graph[front_node]) {
+            if (!visited[node]) {
+                if (capacity[front_node][node] -flow[front_node][node] > 0) {
+                    _queue.push(node);
+                    visited[node] = true;
+                    parent[node] = front_node;
                 }
             }
         }
     }
+}
 
-    if(!visited[d])
-        return 0;
-
+int flow_on_bfs(const int source, const int dest){
     vector<int> path;
-    int x = d;
-    
-    while(x!=0){
-        path.push_back(x);
-        x=p[x];
+
+    reset_visited_parent();
+    bfs(source);
+
+    // fout<<visited[dest]<<endl;
+    if (!visited[dest]) return 0;
+    // fout<<"aici\n";
+    int len=0;
+    int nod =dest;
+    while (nod) {
+        path.emplace_back(nod);
+        nod = parent[nod];
+        len+=1;
+    }
+    reverse(path.begin(), path.end());
+
+    int min_capacity = maxInt;
+
+    for (int i=0;i<len-1;i+=1) {
+        const int node1 = path[i];
+        const int node2 = path[i+1];
+        min_capacity = min(min_capacity, capacity[node1][node2]-flow[node1][node2]);
     }
 
-    //reverse(path.begin(),path.end());
-    ranges::reverse(path);
-    int flow=1e9;
-    for(int i = 0; i<path.size()-1;i+=1){
-        const int x1 = path[i];
-        int y = path[i+1];
-        flow = min(flow, capacity[x1][y]-flux[x1][y]);
+    for (int i=0;i<len-1;i+=1) {
+        const int node1 = path[i];
+        const int node2 = path[i+1];
+        flow[node1][node2] += min_capacity;
+        flow[node2][node1] -= min_capacity;
     }
-    for(int i=0;i<path.size()-1;i+=1){
-        const int x1 = path[i];
-        const int y = path[i+1];
-        flux[x1][y] += flow;
-        flux[y][x1] -=flow;
-    }
-    return flow;
 
+    return min_capacity;
 }
 
 
 int main(){
-    int m,n;
-    fin>>n>>m;
-    for(int i = 1;i<=m;i+=1){
-        int x,y,c;
-        fin>>x>>y>>c;
-        capacity[x][y]=c;
-        graph[x].push_back(y);
+    int maxFlow=0;
+
+    read_graph();
+
+    while (true) {
+        const int flow_on_each_step = flow_on_bfs(1, n);
+        if (!flow_on_each_step) break;
+        maxFlow += flow_on_each_step;
     }
 
-    int maxFlow = 0;
-    while (true){
-        const int flow= bfs(1,n);
-        if(flow == 0){
-            break;
-        }
-        maxFlow += flow;
-    }
     fout<<maxFlow;
     return 0;
 }
